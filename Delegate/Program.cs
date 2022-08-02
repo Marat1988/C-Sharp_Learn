4ecc8a6a7c6060b06198413ea0d70e029dbdc6d2
Marat <pirat03071988@mail.ru>
2022-08-02 22:29:45 +0200

    Update

---
+++ Delegate/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        delegate int Operation(int x, int y); //Объявление делегата
+        delegate void Handler(string message);
+        class Account
+        {
+            private int _sum;
+            private Handler _del;
+            public Account(int sum)
+            {
+                _sum = sum;
+            }
+            public void registerHandler(Handler del)
+            {
+                _del += del;
+            }
+            public void unRegisterHandler(Handler del)
+            {
+                _del -= del;
+            }
+            public void Put(int sum)
+            {
+                _sum += sum;
+                if (_del != null)
+                    _del($"На ваш счет поступило {sum}");
+            }
+
+            public void withDraw(int sum)
+            {
+                if (_sum >= sum)
+                {
+                    _sum -= sum;
+                    if (_del != null)
+                        _del($"С вашего счета снято {sum}");
+                }
+                else
+                {
+                    if (_del != null)
+                        _del($"На счете недостаточно средств");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Result(3, 4, new Operation(Sum)));
+            Console.WriteLine(Result(3, 4, new Operation(Mult)));
+            Account account = new Account(100);
+            account.registerHandler(Message);
+            account.registerHandler(colorMessage);
+            account.Put(150);
+            account.Put(50);
+            account.withDraw(100);
+            account.unRegisterHandler(colorMessage);
+            account.withDraw(400);
+            Console.ReadKey();
+        }
+        static int Result(int x, int y, Operation operation) //Передача делегата в качестве параметра в функцию
+        {
+            return operation(x, y);
+        }
+        static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        static int Mult(int x, int y)
+        {
+            return x * y;
+        }
+        static void Message(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void colorMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Delegate/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Delegate")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Delegate")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b60e2291-2856-4c49-b318-0b7c96548630")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
