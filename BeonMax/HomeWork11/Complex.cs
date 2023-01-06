252e3cc1a1decec14efc7b0dd2d6ca0fb17b1bd3
Marat <pirat03071988@mail.ru>
2023-01-06 16:26:06 +0200

    UPDATE

---
+++ BeonMax/HomeWork11/Complex.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork11
+{
+    public class Complex
+    {
+        public double Imaginagy { get; private set; }
+        public double Real { get; private set; }
+
+        public Complex(double imagingy, double real)
+        {
+            this.Imaginagy = imagingy;
+            this.Real = real;
+        }
+
+        private Complex() { }
+
+        public Complex Plus(Complex other)
+        {
+            var complex = new Complex();
+            complex.Imaginagy = other.Imaginagy + Imaginagy;
+            complex.Real = other.Real + Real;
+            return complex;
+        }
+
+        public Complex Minus(Complex other)
+        {
+            var complex = new Complex();
+            complex.Imaginagy = other.Imaginagy - Imaginagy;
+            other.Real = other.Real - Real;
+            return complex;
+        }
+
+    }
+}
