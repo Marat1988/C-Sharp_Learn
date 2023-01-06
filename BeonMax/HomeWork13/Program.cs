46a13aca3aba4f573fd0922f0e024baa2ed43111
Marat <pirat03071988@mail.ru>
2023-01-07 00:42:18 +0200

    UPDATE

---
+++ BeonMax/HomeWork13/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+
+/*Практическое задание "Крестики-Нолики"
+ 
+А вот и новое домашнее задание. Непростое. Но вполне реализуемое.
+
+Вы попробуете реализовать игру в крестики-нолики размером 3х3 - самые что ни на есть обыкновенные.
+
+Сделайте метод, который выводит на каждом ходу текущее положение с линейками, крестиками и ноликами
+(используйте буквы X и O в качестве крестиков и ноликов) - так игрокам будет удобнее ориентироваться.
+
+Также вам понадобится реализовать способ проверки наличия выигрышной комбинации. Подсказка: договоримся,
+что клетки поля будут пронумерованы от 0 до 8 и пользователи будут вводить индекс поля, чтобы поставить там крестик или нолик.
+
+Для упрощения - тот кто ходит первым - ставит крестик.
+ */
+
+namespace HomeWork13
+{
+    class Program
+    {
+        private static TicTacToeGame g = new TicTacToeGame();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetPrintableState());
+            while (g.GetWinner() == Winner.GameIsUnfinished)
+            {
+                int index = int.Parse(Console.ReadLine());
+                g.MakeMove(index);
+
+                Console.WriteLine();
+                Console.WriteLine(GetPrintableState());
+            }
+            Console.WriteLine($"Reult: {g.GetWinner()}");
+            Console.ReadLine();
+        }
+
+        static string GetPrintableState()
+        {
+            var sb = new StringBuilder();
+            for (int i = 1; i <= 7; i += 3)
+            {
+                sb.AppendLine("     |     |      |")
+                    .AppendLine($"  {GetPrintableChar(i)}  |  {GetPrintableChar(i + 1)}  |  {GetPrintableChar(i + 2)}   |")
+                    .AppendLine("_____|_____|______|");
+            }
+            return sb.ToString();
+        }
+
+        static string GetPrintableChar(int index)
+        {
+            State state = g.GetState(index);
+            if (state == State.Unset)
+            {
+                return index.ToString();
+            }
+            else
+            {
+                return state == State.Cross ? "X" : "O";
+            }
+        }
+    }
+}
---
+++ BeonMax/HomeWork13/TicTacToeGame.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork13
+{
+    public enum Winner
+    {
+        Crosses,
+        Zeroes,
+        Draw,
+        GameIsUnfinished
+    }
+
+    public enum State
+    {
+        Cross,
+        Zero,
+        Unset
+    }
+
+    public class TicTacToeGame
+    {
+        private readonly State[] board = new State[9];
+        public int MovesCounter { get; private set; }
+
+        public TicTacToeGame()
+        {
+            for (int i = 0; i < board.Length; i++)
+            {
+                board[i] = State.Unset;
+            }
+        }
+
+        public void MakeMove(int index)
+        {
+            board[index - 1] = MovesCounter % 2 == 0 ? State.Cross : State.Zero;
+            MovesCounter++;
+        }
+
+        public State GetState(int index)
+        {
+            return board[index - 1];
+        }
+
+        public Winner GetWinner()
+        {
+            return GetWinner(1, 4, 5, 2, 5, 8, 3, 6, 9,
+                1, 2, 3, 4, 5, 6, 7, 8, 9,
+                1, 5, 9, 3, 5, 7);
+        }
+
+        private Winner GetWinner(params int[] indexes)
+        {
+            for (int i = 0; i < indexes.Length; i+=3)
+            {
+                bool same = AreSame(indexes[i], indexes[i + 1], indexes[i + 2]);
+                if (same)
+                {
+                    State state = GetState(indexes[i]);
+                    if (state != State.Unset)
+                    {
+                        return state == State.Cross ? Winner.Crosses : Winner.Zeroes;
+                    }
+                }
+            }
+            if (MovesCounter < 9)
+            {
+                return Winner.GameIsUnfinished;
+            }
+            else
+            {
+                return Winner.Draw;
+            }
+        }
+
+        private bool AreSame(int a, int b, int c)
+        {
+            return GetState(a) == GetState(b) && GetState(a) == GetState(c);
+        }
+    }
+}
