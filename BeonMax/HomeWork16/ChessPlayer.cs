e8c5980d4ef368a401fbd942d9fd3e4c2a8e96c6
Marat <pirat03071988@mail.ru>
2023-01-22 16:31:43 +0200

    UPDATE

---
+++ BeonMax/HomeWork16/ChessPlayer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork16
+{
+    public class ChessPlayer
+    {
+        public string Country { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthYear { get; set; }
+        public int Rating { get; set; }
+        public int Id { get; set; }
+
+        public override string ToString()
+        {
+            return $"Full Name: {FirstName + " " + LastName}, Rating = {Rating}, from {Country}," +
+                $"born in {BirthYear}";
+        }
+
+        public static ChessPlayer ParseFideCsv(string line)
+        {
+            string[] parts = line.Split(';');
+
+            int BirthYear = int.Parse(parts[6]);
+            return new ChessPlayer()
+            {
+                Id = int.Parse(parts[0]),
+                LastName = parts[1].Split('\t')[0].Trim(),
+                FirstName = parts[1].Split('\t')[1].Trim(),
+                Country = parts[3],
+                Rating = int.Parse(parts[4]),
+                BirthYear = int.Parse(parts[6])
+            };
+        }
+    }
+}
---
+++ BeonMax/HomeWork16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork16
+{
+    /*Используя файл с ТОП100 шахматных игроков, найти всех игроков из России и отсортировать их по году рождения по возрастанию.*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var players = File.ReadAllLines(@"Top100ChessPlayers.csv")
+                              .Skip(1)
+                              .Select(ChessPlayer.ParseFideCsv)
+                              .Where(player => player.Country == "RUS")
+                              .OrderBy(player => player.BirthYear)
+                              .ToList();
+            foreach (var player in players)
+            {
+                Console.WriteLine(player);
+            }
+            Console.ReadLine();
+        }
+    }
+}
