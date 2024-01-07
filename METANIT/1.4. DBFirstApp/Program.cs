b57453c795d953a57a3de6c7ee2616a47806753c
Marat <pirat03071988@mail.ru>
2024-01-07 23:37:04 +0200

    UPDATE

---
+++ METANIT/1.4. DBFirstApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace DBFirstApp
+{
+    class Program
+    {
+        static  void Main(string[] args)
+        {
+            /*using(helloappContext db = new helloappContext())
+            {
+                bool isCreated = db.Database.EnsureCreated();
+                //bool isCreated2 = await db.Database.EnsureCreatedAsync()
+                if (isCreated) Console.WriteLine("База данных была создана");
+                else Console.WriteLine("База данных существует");
+            }*/
+
+            /*using (helloappContext db = new helloappContext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+                //асинхронная версия
+                //await db.Database.EnsureCreatedAsync();
+                //await db.Database.EnsureDeletedAsync();
+            }*/
+            using (helloappContext db = new helloappContext())
+            {
+                bool isAvalaible = db.Database.CanConnect();
+                //bool isAvalaible2 = await db.Database.CanConnectAsync()
+                if (isAvalaible) Console.WriteLine("База данных доступна");
+                else Console.WriteLine("База данных не доступна");
+            }
+        }
+    }
+}
---
+++ METANIT/1.4. DBFirstApp/helloappContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace DBFirstApp
+{
+    public partial class helloappContext : DbContext
+    {
+        public helloappContext()
+        {
+            Database.EnsureDeleted(); //гарантируем, что бд удалена
+            Database.EnsureCreated(); //гарантируем, что БД будет создана
+        }
+
+        public helloappContext(DbContextOptions<helloappContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlite("Data Source=helloapp.db");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
